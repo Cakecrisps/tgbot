c79e05ff5f3242d1a00570a3165a1d668f5cc94c
Cakecrisps <100094047+Cakecrisps@users.noreply.github.com>
2024-06-06 20:13:14 +0300

    Update imgcollect.py

--- imgcollect.py
+++ imgcollect.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 @app.route("/",methods = ["POST"])
 def st():
     js = request.get_json()
-    print(js)
     call = js['body']
     size = js['size']
     scale = js['scale']
